ff5a59e18c3cd079d53f884865fea7a7d4333a05
voxServalG <41219915+voxServalG@users.noreply.github.com>
2022-04-13 19:42:43 +0800

    Last check from 316/1040

--- trim_silence_test.py
+++ trim_silence_test.py
@@ -1,7 +1,37 @@
 import librosa
+from librosa.display import specshow
+import numpy as np
+import soundfile as sf
+import matplotlib.pyplot as plt
+import os
+import sys
+
+
 def trim_silence(wav, top_db, frame_length, hop_length):
     return librosa.effects.trim(wav, top_db=top_db, frame_length=frame_length, hop_length=hop_length)
 
 
+INPUT = 'E:\\python_PROJECTS\\cantonese_process\\220411_1835.wav'
+OUTPUT = 'E:\\python_PROJECTS\\cantonese_process\\result.wav'
+rate = 44100
+
+
+def get_db(wav, sr):
+    S = np.abs(librosa.stft(wav))
+    plt.figure()
+    plt.subplot(2, 1, 1)
+    specshow(S ** 2, sr=sr, y_axis='log')
+    plt.colorbar()
+    plt.title('Power spectogram')
+    plt.subplot(2, 1, 2)
+    specshow(librosa.power_to_db(S ** 2, ref=np.max), sr=sr, y_axis='log', x_axis='time')
+    plt.colorbar(format='%+2.0f dB')
+    plt.title('Log-Power spectogram')
+    plt.set_cmap('autumn')
+    plt.tight_layout()
+    plt.show()
 if __name__ == '__main__':
-    pass
+    wav = librosa.core.load(INPUT, sr=44100)[0]
+    # get_db(wav, rate)
+    result = librosa.effects.trim(wav, top_db=60, frame_length=2048, hop_length=512)[0]
+    sf.write(OUTPUT, result, rate, format='wav')
